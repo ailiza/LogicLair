b5324d8eeaf8066cc7f99423342c58569b2c2d53
ailiza <annie.ilizarov@gmail.com>
2023-11-27 10:46:28 -0500

    added permute and edited coinChange

--- DynamicProgramming/coinChange.js
+++ DynamicProgramming/coinChange.js
@@ -24,111 +24,19 @@ Output: 0
 */
 
 // Time: O(amount * denomination count of coins) | Space: O(amount) for the memoization table
-var coinChange = function(coins, amount) {
+var coinChange = function(coins, totalAmount) {
     const MAX = Number.MAX_SAFE_INTEGER;
-    const dp = new Array(amount + 1).fill(MAX);
+    const dp = new Array(totalAmount + 1).fill(MAX);
     dp[0] = 0;
     
-    
-    for (let amountIndex = 1; amountIndex < dp.length; amountIndex++) {
+    for (let currAmount = 1; currAmount < totalAmount + 1; currAmount++) {
         for (const coin of coins) {
-            if (amountIndex - coin >= 0) {
-                const possibleMinNumOfCoins = dp[amountIndex - coin] + 1;
-                dp[amountIndex] = Math.min(dp[amountIndex], possibleMinNumOfCoins);                
+            if (currAmount - coin >= 0) {
+                const change = dp[currAmount - coin] + 1
+                dp[currAmount] = Math.min(dp[currAmount], change)
             }
         }
     }
-    
-    if (dp[amount] === MAX) return -1;
-    return dp[amount];
+    if (dp[totalAmount] === MAX) return -1;
+    return dp[totalAmount];
 };
-
-
-/*
- - building up the solution from 0 to 350
- - coins consist of [1,5, 10, 25, 100]
-    - inner loop we are iterating through the coins
-     = dp[350] = infinity
-      - coin = 1
-        - a = 350, coin = 1, amount = 350
-        - amount -1 == 350 - 1 == 349...is dp[349] + 1 OR what's currently in dp[350]
-
-      - coin = 5
-        - a = 350, coin = 5, amount 350
-        - 345 is dp[345] + 1 OR what's currently dp[350]
-
-     - coin = 10
-        - is dp[340] + 1 OR what's currently in dp[350]
-
-
-
-
-
-*/
-var coinChange = function(coins, amount) {
-    const MAX = Number.MAX_SAFE_INTEGER; 
-    const dp = new Array(amount + 1).fill(MAX);
-    dp[0] = 0; // this is saying we don't need any coins to add up to the amount 0
-    
-    for (let a = 1; a < amount + 1; a++) { //each index in dp starting at 1, where a is the possible amount
-        for (const coin of coins) {
-            if (a - coin >= 0) { //if it's negative then it's not a possibility because we're subtracting
-                const change = dp[a - coin] + 1;
-                dp[a] = Math.min(dp[a], change);
-            }
-        }
-    }
-    
-    if (dp[amount] === MAX) return -1; // this is in case where you use up all the coins, but cannot add up to the amount.
-    return dp[amount];
-};
-
-// [1,3,4,5] 7
-
-// function minCoins(coins, target) {
-//     let min = Infinity;
-
-//     function explore(tempArr, idx) {
-//         let tempSum = getSum(tempArr);
-//         if (tempSum > target) return;
-//         if (tempSum === target) {
-//             min = Math.min(tempArr.length, min);
-//         }
-
-//         for (let i = idx; i < coins.length; i++) {
-//             let el = coins[i];
-//             tempArr.push(el);
-//             explore(tempArr, i);
-//             tempArr.pop();
-//         }
-//     }
-
-//     explore([], 0)
-
-//     return min;
-
-//     function getSum(arr) {
-//         return arr.reduce((acc, curr) => acc + curr, 0);
-//     }
-// }
-
-
-/*
-
-"each index in dp starting at 1, where a is the possible amount"
-Concrete Example:
-    "let's make 3.50 with pennies, nickels, quarters, dollars"
-    amt = 350
-    coins [1, 5, 25, 100]
-
-
-    - Initialize each of the dp with "Infinity"
-
-    dp[1] = "what is the minimum number of coins to make 1" = 1
-    dp[2] = "...2" -> "Infinity"
-    dp[3] = "...3"
-    ...
-    dp[350] "...350" -> solution
-
-
-*/
